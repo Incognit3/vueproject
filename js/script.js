94bcfeea961b1783924995b3778f2333e4e162dd
Incognit3 <mockvich88@gmail.com>
2019-02-05 17:25:31 +0300

    vuePROJJect

--- js/script.js
+++ js/script.js
@@ -1,69 +1,36 @@
-var demo = new Vue({
-    el: '#main',
-    data: {
-        searchString: "",
+var app = new Vue({
+    el: '#app',
+    data: { paradiseOne: 0,
+            nortsideTwo: 0
+           
+        },
+        methods: {
+        autz: function() {
+             if (parseInt(this.paradiseOne) > 0) return "green";
+             if (parseInt(this.paradiseOne) < 0) return "red";
+              
+        },
 
-        // Модель данных. Эти данные в другом случае передавались бы ajax'ом, но сейчас описаны прямо здесь для упрощения.
-
-        articles: [
-            {
-                "title": "27 сайтов с задачками для оттачивания навыков программирования",
-                "url": "https://proglib.io/p/27-puzzle-websites-to-sharpen-your-skills/",
-                "image": "https://proglib.io/wp-content/uploads/2016/10/coding-time-100x70.jpg"
-            },
-            {
-                "title": "Готовимся к собеседованию в Google: 8 месяцев непрерывной работы",
-                "url": "https://proglib.io/p/8-month-for-google-interview/",
-                "image": "https://proglib.io/wp-content/uploads/2017/01/0_b11a3_8ff3ef7b_XL-100x70.jpg"
-            },
-            {
-                "title": "15 материалов по разработке игр",
-                "url": "https://proglib.io/p/gamedev-compilation/",
-                "image": "https://proglib.io/wp-content/uploads/2017/05/gamedev-100x70.png"
-            },
-            {
-                "title": "10 лучших видеокурсов для изучения Linux",
-                "url": "https://proglib.io/p/linux-courses/",
-                "image": "https://proglib.io/wp-content/uploads/2017/01/ruqlwta5wqy-100x70.jpg"
-            },
-            {
-                "title": "Учебный план по осваиванию современного JavaScript",
-                "url": "https://proglib.io/p/learning-modern-javascript/",
-                "image": "https://proglib.io/wp-content/uploads/2016/12/1raWO3dhM4jMjf9VY-kZzNg-100x70.png"
-            },
-            {
-            "title": "5 практических примеров использования Vue.js",
-                "url": "https://proglib.io/p/5-vue-js-examples/",
-                "image": "https://proglib.io/wp-content/uploads/2016/10/coding-time-100x70.jpg"
-            },
-            {
-                "title": "GitHub",
-                "url": "https://github.com/",
-                "image": "https://proglib.io/wp-content/uploads/2017/01/0_b11a3_8ff3ef7b_XL-100x70.jpg"
-            }
-        ]
-
-    },
-    computed: {
-        // Вычисленное свойство, которое содержит только те статьи, которые соответствуют searchString.
-        filteredArticles: function () {
-            var articles_array = this.articles,
-                searchString = this.searchString;
-
-            if(!searchString){
-                return articles_array;
-            }
-
-            searchString = searchString.trim().toLowerCase();
-
-            articles_array = articles_array.filter(function(item){
-                if(item.title.toLowerCase().indexOf(searchString) !== -1){
-                    return item;
-                }
-            })
-
-            // Возвращает массив с отфильтрованными данными.
-            return articles_array;;
+         autz2: function() {
+             if (parseInt(this.nortsideTwo) > 0) return "green";
+             if (parseInt(this.nortsideTwo) < 0) return "red";
+              
+        },
+        total:function() {
+            
+            return parseInt(this.paradiseOne)+parseInt(this.nortsideTwo);
+        },
+        colbigo:function(){
+            console.log(this.tot);
+            if(parseInt(this.paradiseOne)+parseInt(this.nortsideTwo)>0) return "grintxt";
+            if(parseInt(this.paradiseOne)+parseInt(this.nortsideTwo)<0) return "redtxt";
         }
+
+        
     }
+
 });
+
+
+       
+   
