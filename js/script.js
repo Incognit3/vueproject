e8f2003bf5116f4f1c06b1e22fcb49ed07ec6dd9
pegr29 <pegr29@mail.ru>
2019-02-12 17:18:48 +0300

    lifeguard

--- js/script.js
+++ js/script.js
@@ -1,40 +1,119 @@
-  var app = new Vue({
-        el: '#app',
-        data: {
-            pegers: '',
-            numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        },
-        methods: {
-            checkInfo: function() {    
-                console.log(this.pegers);
-                    if (this.numbers.indexOf(this.pegers) == -1) {
-                        this.numbers.indexOf(this.pegers);
-                        this.numbers.push(this.pegers); }
-                    
-                    else {
-                    console.log(false); 
+// var data = {
+//         header: 'Counter Program',
+//         count:0
+// };
+// Vue.component('counter', {
+//     data: function(){
+//         return data
+//     },
+//     template:`<div><h2>{{header}}</h2>
+//                 <button v-on:click="count++">+</button>
+//                 <span>{{count}}</span>
+//             </div>`,
+//     methods:{
+//         increase:function(){
+//             this.count++;
+//         }
+//     }
+// });
+// new Vue({
+//     el: "#app"
+// });
 
-            }
+// Vue.component('section-header',{
+//     template:'<h2>Header</h2>'
+// });
+// var comp1 = {
+//     template:'<div>Content 1</div>'
+// };
+// var comp2 = {
+//     template:'<div>Content 2</div>'
+// };
+// var footer = {
+//     template:'<p><b>Footer</b></p>'
+// };
+// new Vue({
+//     el: "#app1",
+//     components:{
+//         'section-content':comp1,
+//         'section-footer':footer
+//     }
+// });
+// new Vue({
+//     el: "#app2",
+//     components:{
+//         'section-content':comp2
+//     }
+// });
+
+// Vue.component('counter', {
+//     data: function(){
+//         return {
+//             header: 'Counter Program',
+//             count:0
+//         }
+//     },
+//     template:`<div><h2>{{header}}</h2>
+//                 <button v-on:click="increase">+</button>
+//                 <span>{{count}}</span>
+//             </div>`,
+//     methods:{
+//         increase:function(){
+//             this.count++;
+//         }
+//     }
+// });
+// new Vue({
+//     el: "#app3"
+// });
+
+Vue.component('counter', {
+    data: function(){
+        return {
+            header: 'Social Program.',
+            count: 0,
+            paradiseOne: 0,
+            nortsideTwo: 0
         }
-    }
-})
-
-var app = new Vue({
-    el: '#appTwo',
-    data: {
-        login: '',
-        password: '',
-        date: '',
-        age: 22,
-        email: ''
-       
     },
+    template:`<div><h2>{{header}}</h2>
+                <button v-on:click="colbigo">+</button>
+                <span>{{count}}</span>
+                 <input type="text" v-model="paradiseOne" v-bind:class="autz()">
+   			   <input type="text" v-model="nortsideTwo" v-bind:class="autz2()">
+            </div>`,
     
-    methods: {
-        loginInfo:function() {
+    methods:{
+        increase:function() {
+            this.count++;
+    	
+    	},
+        autz: function() {
+             if (parseInt(this.paradiseOne) > 0) return "green";
+             if (parseInt(this.paradiseOne) < 0) return "red";
+              
+        },
 
-            // # code...
+         autz2: function() {
+             if (parseInt(this.nortsideTwo) > 0) return "green";
+             if (parseInt(this.nortsideTwo) < 0) return "red";
+              
+        },
+        total:function() {
+           	return parseInt(this.paradiseOne)+parseInt(this.nortsideTwo);
         
+        },
+        colbigo:function(){
+            console.log(this.tot);
+            this.count = Number(this.paradiseOne)+Number(this.nortsideTwo);
+            
         }
+
     }
+
+});
+
+new Vue({
+    el: "#app4"
+
 });
